880e11e199764881b7eae98fffe7ae78e0c78bb6
JanSidelnik2097117 <j.sidelnik@student.tue.nl>
2026-05-27 19:43:42 +0200

    done

--- src/add_landmark_measurement.py
+++ src/add_landmark_measurement.py
@@ -7,9 +7,27 @@ PRIOR_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.1, 0.1, 0.05]))  # (x
 ODOMETRY_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.2, 0.2, 0.1]))  # (dx, dy, dtheta)
 MEASUREMENT_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.05, 0.1]))  # (bearing, range)
 
+
 def add_landmark_measurement(graph, initial_estimate, result):
-    # Determine the correct rotation (bearing) and distance from X(4) to L(2) 
-    # rotation = 
-    # distance = 
-    graph.add(gtsam.BearingRangeFactor2D(X(4), L(2), gtsam.Rot2.fromDegrees(rotation), distance, MEASUREMENT_NOISE))
+    pose4 = result.atPose2(X(4))
+    landmark2 = result.atPoint2(L(2)) 
+
+    dx = landmark2[0] - pose4.x()
+    dy = landmark2[1] - pose4.y()
+
+    distance = math.sqrt(dx**2 + dy**2)
+
+    bearing_rad = math.atan2(dy, dx) - pose4.theta()
+
+    rotation = math.degrees(bearing_rad)
+
+    graph.add(
+        gtsam.BearingRangeFactor2D(
+            X(4), L(2),
+            gtsam.Rot2.fromDegrees(rotation),
+            distance,
+            MEASUREMENT_NOISE
+        )
+    )
+
     return graph
--- src/add_pose.py
+++ src/add_pose.py
@@ -9,30 +9,28 @@ ODOMETRY_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.2, 0.2, 0.1]))  # 
 MEASUREMENT_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.05, 0.1]))  # (bearing, range)
 
 
+
+
 def add_pose(graph, initial_estimate):
-
-    # Odometry from x(4) to x(5)
-
     relative_motion = gtsam.Pose2(
-        2.0,          # move forward 2m
-        0.0,
-        math.pi / 2   # rotate 90deg 
+        math.sqrt(2),
+        math.sqrt(2),
+        math.pi / 2
     )
 
     graph.add(
         gtsam.BetweenFactorPose2(
-            X(4), X(5),
+            X(3), X(4),
             relative_motion,
             ODOMETRY_NOISE
         )
     )
 
-    # Initial estimate for x(5)
+    pose3_ideal = gtsam.Pose2(4.0, 0.0, 0.0)
 
-    pose4 = initial_estimate.atPose2(X(4))
+    pose4_guess = pose3_ideal.compose(relative_motion)
 
-    pose5_guess = pose4.compose(relative_motion)
-
-    initial_estimate.insert(X(5), pose5_guess)
+    initial_estimate.insert(X(4), pose4_guess)
 
     return graph, initial_estimate
+
